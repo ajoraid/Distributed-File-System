bdb3d04ba2ce0148be82abb57adc3739a93def80
Adnan Joraid <adnanjoraidca@gmail.com>
2024-12-16 14:29:21 -0500

    Edit make_protos.sh

--- Client/Sources/make_protos.sh
+++ Client/Sources/make_protos.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-PROTO_DIR="./proto-src"
-OUTPUT_DIR="./proto-src"
+PROTO_OUTPUT_DIR="./proto-src"
 
 echo "Cleaning up old generated files"
-find "$OUTPUT_DIR" -type f \( -name "*.pb.swift" -o -name "*.grpc.swift" \) -delete
+find "$PROTO_OUTPUT_DIR" -type f \( -name "*.pb.swift" -o -name "*.grpc.swift" \) -delete
 
 protoc \
-  --swift_out="$OUTPUT_DIR" \
-  --grpc-swift_out="$OUTPUT_DIR" \
-  -I"$PROTO_DIR" \
-  "$PROTO_DIR/dfsclient.proto"
+  --swift_out="$PROTO_OUTPUT_DIR" \
+  --grpc-swift_out="$PROTO_OUTPUT_DIR" \
+  -I"$PROTO_OUTPUT_DIR" \
+  "$PROTO_OUTPUT_DIR/dfsclient.proto"
 
-echo "Proto files generated in $OUTPUT_DIR"
+echo "Proto files generated in $PROTO_OUTPUT_DIR"
